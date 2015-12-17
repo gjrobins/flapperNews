18674139ab40cc90f9cf7a5412b8c5c275b5a9d3
gjrobins <robinson.grant11@gmail.com>
2015-12-17 08:13:55 -0500

    Section 2.2 Submitting Links

--- app.js
+++ app.js
@@ -14,8 +14,12 @@ app.controller('MainCtrl', [
 		
 		$scope.addPost = function(){
 			if(!$scope.title || $scope.title === ''){return;}
-			$scope.posts.push({title: $scope.title, upvotes: 0});
+			$scope.posts.push({
+				title: $scope.title,
+				link: $scope.link,
+				upvotes: 0});
 			$scope.title = '';
+			$scope.link = '';
 		};
 		
 		$scope.incrementalUpvotes = function(post) {
