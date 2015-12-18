060729b31bc4a718132f0005bc932ec79cbffe2f
gjrobins <robinson.grant11@gmail.com>
2015-12-17 20:19:15 -0500

    Angular Complete & Fully Functional

--- app.js
+++ app.js
@@ -33,34 +33,26 @@ app.controller('MainCtrl', [
 	function($scope, posts){
 		$scope.test = 'Hello World!';
 		$scope.posts = posts.posts;
-		// $scope.posts = [
-		// 	{title: 'post 1', upvotes: 5},
-		// 	{title: 'post 2', upvotes: 2},
-		// 	{title: 'post 3', upvotes: 15},
-		// 	{title: 'post 4', upvotes: 9},
-		// 	{title: 'post 5', upvotes: 4}
-		// ];
 		
-		$scope.addPost = function(){
-			if(!$scope.title || $scope.title === ''){return;}
-			console.log('h');
-			$scope.posts.push({
-				title: $scope.title,
-				link: $scope.link,
-				upvotes: 0,
-				comments: [
-					{author: 'Joe', body: 'Cool post!', upvotes: 0},
-					{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-  				]
-			});
-			console.log($scope.title);
-			$scope.title = '';
-			$scope.link = '';
-		};
-		
-		$scope.incrementalUpvotes = function(post) {
-			post.upvotes += 1;
-		};
+	$scope.addPost = function(){
+		if(!$scope.title || $scope.title === ''){return;}
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes: 0,
+			comments: [
+				{author: 'Joe', body: 'Cool post!', upvotes: 1},
+				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+			]
+		});
+		$scope.title = '';
+		$scope.link = '';
+	};
+	
+	$scope.incrementalUpvotes = function(post) {
+		console.log('h');
+		post.upvotes += 1;
+	};
 }]);
 
 app.controller('PostCtrl', [
@@ -68,5 +60,21 @@ app.controller('PostCtrl', [
 	'$stateParams',
 	'posts',
 	function($scope, $stateParams, posts){
-		$scope.posts = posts.posts[$stateParams.id];
+		$scope.post = posts.posts[$stateParams.id];
+		console.log($scope.post);
+
+	$scope.addComment = function() {
+		if($scope.body === '') {return;}
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: 'user',
+			upvotes: 0
+		});
+		$scope.body = '';
+	};
+	
+	$scope.incrementalUpvotes = function(comment) {
+		comment.upvotes += 1;
+	};	
+
 }]);
